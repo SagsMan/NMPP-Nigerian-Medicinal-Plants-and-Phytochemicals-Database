e4fc381c0f79380c50ee45e91bcf5c02b993b7ac
Sagiru Garba <122167371+SagsMan@users.noreply.github.com>
2026-06-02 01:27:42 +0100

    Fix compunds_server_processing.php: use correct DB columns
    Previous fix used wrong column names (pubchem instead of compound_code
    for Compound ID, missing synonyms column). Now using correct mapping:
    - compound_code -> Compound ID (NMPC-xxx)
    - compounds_name -> Compound Name (with link)
    - synonyms -> Synonyms (was missing, compound_code was wrongly used)
    - pubchem -> Pubchem ID
    - molecular_formula -> Molecular Formula
    - id -> No of Sources (count via Get_Count_Nums_Of_Species_Have_This_Compunds)

--- inc/compunds_server_processing.php
+++ inc/compunds_server_processing.php
@@ -9,12 +9,12 @@ $table = 'compounds_cati_tbl';
 $primaryKey = 'id';
 
 $columns = [
-    ['db' => 'pubchem', 'dt' => 0],           // Compound ID (first column)
-    ['db' => 'compounds_name', 'dt' => 1],     // Compound Name (second column)
-    ['db' => 'compound_code', 'dt' => 2],      // Synonyms (third column)
-    ['db' => 'molecular_formula', 'dt' => 3],  // Pubchem ID
-    ['db' => 'smiles', 'dt' => 4],             // Molecular Formula
-    ['db' => 'id', 'dt' => 5],                 // No of Sources
+    ['db' => 'compound_code', 'dt' => 0],      // Compound ID (NMPC-xxx) - first column
+    ['db' => 'compounds_name', 'dt' => 1],     // Compound Name - second column
+    ['db' => 'synonyms', 'dt' => 2],           // Synonyms - third column
+    ['db' => 'pubchem', 'dt' => 3],            // Pubchem ID - fourth column
+    ['db' => 'molecular_formula', 'dt' => 4],  // Molecular Formula - fifth column
+    ['db' => 'id', 'dt' => 5],                 // No of Sources - sixth column
     ['db' => 'cati_link', 'dt' => 'cati_link'],
 ];
 
@@ -37,10 +37,10 @@ if (isset($results['data']) && count($results['data']) > 0) {
         $compoundId = $row[5];
         $compoundLink = $row['cati_link'];
 
-        // 1. Link Compound Name (now at position 1)
+        // 1. Link Compound Name (position 1)
         $results['data'][$key][1] = "<a href='compounds-detail?compound=" . $compoundLink . "'>" . $row[1] . "</a>";
 
-        // 2. Display Number of Sources directly (instead of View Details button)
+        // 2. Display Number of Sources (count of species that have this compound)
         $sourcesObj = $AdminTask->Get_Count_Nums_Of_Species_Have_This_Compunds($compoundId);
         $results['data'][$key][5] = $sourcesObj ? (int)$sourcesObj->totalCount : 0;
     }
